ffdc5730427a0cb2eb49b1012084c84ff62f6203
privateos <privateos@foxmail.com>
2020-09-20 14:15:38 +0800

    concat excel in row

---
+++ read_write_excel/concat_in_row.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import os
+old_file_name = 'D:/shan/repo/pandas/read_write_excel/example_multisheets.xlsx'
+new_file_name = 'D:/shan/repo/pandas/read_write_excel/example_multisheets_new_concat_in_row.xlsx'
+writer = pd.ExcelWriter(new_file_name)
+reader = pd.ExcelFile(old_file_name)
+
+dfs = []
+for sheet in reader.sheet_names:
+    df = reader.parse(sheet_name=sheet)
+    df['平均成绩'] = (df['语文'] + df['数学'])/2
+    dfs.append(df)
+concated_dfs = pd.concat(dfs, axis=0)
+concated_dfs.to_excel(writer, sheet_name='一班和二班', index=False)
+
+
+writer.save()
+writer.close()
